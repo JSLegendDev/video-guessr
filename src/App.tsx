ad7559aabc92233a87a8c93ca93603a26b738ba2
Nassim's Software <128189209+JSLegendDev@users.noreply.github.com>
2023-04-23 11:58:56 -0400

    started working on main page layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
 function App() {
   return (
     <>
-      <h1 className="text-2xl font-bold underline">
-        Hello world!
-      </h1>
+      <header className="flex justify-center items-center h-20 bg-blue-100">
+        <div className="columns-1">
+          <h1 className="text-2xl font-bold text-center">Video Guessr</h1>
+          <div className="ml-2 mr-2">Guess which videos has more views by looking only at their thumbnails.</div>
+        </div>
+      </header>
+      <div className="flex flex-col items-center">
+        <h1>Test</h1>
+        <h1>Test</h1>
+        <h1>Test</h1>
+      </div>
     </>
   )
 }
